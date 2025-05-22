895dd0e939704eeac5bcbba4a8fa82338b496944
Prajjwal1203 <pmprajjwal12@gmail.com>
2025-05-22 22:46:41 +0530

    fix: upgrade to React 18 for Clerk compatibility

---
+++ next.config.js
@@ -0,0 +1,33 @@
+/** @type {import('next').NextConfig} */
+/*const nextConfig = {
+  images:{
+    domains:['cdn-icons-png.flaticon.com']
+  }
+};
+
+
+export default nextConfig;*/
+
+// next.config.js
+/*module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn-icons-png.flaticon.com',
+      },
+    ],
+  },
+};*/
+// next.config.js
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn-icons-png.flaticon.com',
+      },
+    ],
+  },
+};
+
